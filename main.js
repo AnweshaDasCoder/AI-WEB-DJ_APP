50c93e713be92622c8065e3f117f76bf3bbf0afd
AnweshaDasCoder <95891366+AnweshaDasCoder@users.noreply.github.com>
2022-11-23 13:45:33 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,6 +11,22 @@ function draw() {
 
 song="";
 
+function preload () {
+    song=loadSound("music.mp3");
+}
+
 function play () {
     song.play();
+}
+
+function gotPoses(results) {
+    if(results.length>0) {
+console.log(results);
+leftWristX = results[0].pose.leftWrist.x;
+leftWristY = results[0].pose.leftWrist.y;
+rightWristX = results[0].pose.rightWrist.x;
+rightWristY = results[0].pose.rightWrist.y;
+console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
